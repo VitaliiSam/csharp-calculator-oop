e09d9cc2385534ebbc30f560e67ce532131c2c0a
Vitalii Samchynskyi <samuelvit88@gmail.com>
2025-11-02 19:08:01 +0200

    added calculations to console app

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -5,8 +5,24 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter first value: ");
+        double a = Convert.ToDouble(Console.ReadLine());
 
-        Calculator class1 = new Calculator();
+        Console.WriteLine("Enter second value: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        double add = Calculator.Add(a, b);
+        Console.WriteLine($"Result of adding numbers: {add}");
+
+        double subtract = Calculator.Subtract(a, b);
+        Console.WriteLine($"Result of subtracting numbers: {subtract}");
+
+        double multiply = Calculator.Multiply(a, b);
+        Console.WriteLine($"Result of multiplying numbers: {multiply}");
+
+        double divide = Calculator.Divide(a, b);
+        Console.WriteLine($"Result of dividing numbers: {divide}");
+
+        Console.WriteLine($"Last result: {Calculator.LastResult}");
     }
 }
--- CalculatorLib/Calculator.cs
+++ CalculatorLib/Calculator.cs
@@ -2,21 +2,30 @@
 {
     public class Calculator
     {
+        public static double LastResult {  get; private set; }
         public static double Add (double a, double b)
         {
-            return a + b;
+            LastResult = a + b;
+            return LastResult;
         }
         public static double Subtract (double a, double b)
         {
-            return a - b;
+            LastResult = a - b;
+            return LastResult;
         }
         public static double Multiply (double a, double b)
         {
-            return a * b;
+            LastResult = a * b;
+            return LastResult;
         }
         public static double Divide (double a, double b)
-        { 
-            return a / b; 
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Can't divide by zero");
+            }
+            LastResult = a / b;
+            return LastResult; 
         }
     }
 }
